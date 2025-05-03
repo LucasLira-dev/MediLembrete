2e34c8624cf856cdfcba66c4c57417a45677fa68
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-03 16:19:40 -0300

    criando componente header

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header/Header";
+
+
+export default function Home() {
+  return (
+    <main 
+    className="flex flex-col min-h-screen">
+      <Header />
+    </main>
+  );
+}
---
+++ my-app/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+export const Header = () =>{
+    return(
+        <header className="flex flex-col bg-[#F1F7FF] p-4 shadow-md">
+            <h1 className="text-2xl font-bold text-[#1D4ED8]">           
+                 MediLembrete
+            </h1>
+            <p
+            className="text-[#4B5567] text-[16px] mt-1">
+                Gerencie seus horários de medicamentos com facilidade
+            </p>
+        </header>
+    )
+}
