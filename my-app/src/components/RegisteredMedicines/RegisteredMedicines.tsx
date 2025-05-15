1b8c0cbec12954ad4403b7bf2a60c0ca8c28e274
lucas mendes <lucasmndes.dev@gmail.com>
2025-05-15 16:02:56 -0300

    adicionando popup de editar medicamento

--- my-app/src/components/RegisteredMedicines/RegisteredMedicines.tsx
+++ my-app/src/components/RegisteredMedicines/RegisteredMedicines.tsx
@@ -9,6 +9,17 @@ import { FaRegEdit } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa6";
 
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+
 interface IRegisteredMedicinesProps {
     name: string;
     dosagem: number;
@@ -50,90 +61,131 @@ export const RegisteredMedicines = ({name, dosagem, horariosSalvos} :IRegistered
         calcularProximaDose();
     }, [horariosSalvos]);
 
-    return(
-        
-            <article
-            className="bg-[#1F2937] flex flex-col gap-4 rounded-md p-4 mb-4 w-full data-[deleted=true]:hidden"
-            data-deleted={isDeleted}>
-                <div
-                className="flex justify-between items-center">
-                    <h2
-                    className="mb-1 text-[22px] font-bold px-4"> 
-                        {name} 
-                    </h2>
-                    <RiDeleteBin6Line 
-                    size={16}
-                    className="text-[#F37272] cursor-pointer"
-                    onClick={() => setIsDeleted(true)}
-                    />
-                    
-                </div>
-                
-                <span
-                className="flex items-center gap-1 px-4 text-[#6B7280] text-[14px]"> 
-                    <LuPill
-                    size={14}
-                    />
-                    <p> {dosagem}mg </p>
-                </span>
+    return (
+      <article
+        className="bg-[#1F2937] flex flex-col gap-4 rounded-md p-4 mb-4 w-full data-[deleted=true]:hidden"
+        data-deleted={isDeleted}
+      >
+        <div className="flex justify-between items-center">
+          <h2 className="mb-1 text-[22px] font-bold px-4">{name}</h2>
+          <RiDeleteBin6Line
+            size={16}
+            className="text-[#F37272] cursor-pointer"
+            onClick={() => setIsDeleted(true)}
+          />
+        </div>
 
-                <div
-                className="flex flex-col gap-1 px-4 mb-4">
-                    <h3> Horários: </h3>
-                    <div
-                    className="flex gap-2">
-                    { horariosSalvos.map((horario, index) => (
-                        <div
-                        key={index}
-                        data-tomou={tomei}
-                        className='flex gap-2 justify-center items-center text-[12px] font-medium rounded-full bg-[#1E2C48] p-1 w-[80px] text-[#375cc3] data-[tomou=true]:text-[#16A34A]'>
-                            <CiClock2
-                            className='text-[#3B82F6]'/>
-                             <span> 
-                                {horario}
-                            </span>
-                            {
-                                tomei && (
-                                    <FaCheck
-                                    className='text-[#16A34A]'/>
-                                )
-                            }
-                            
+        <span className="flex items-center gap-1 px-4 text-[#6B7280] text-[14px]">
+          <LuPill size={14} />
+          <p> {dosagem}mg </p>
+        </span>
+
+        <div className="flex flex-col gap-1 px-4 mb-4">
+          <h3> Horários: </h3>
+          <div className="flex gap-2">
+            {horariosSalvos.map((horario, index) => (
+              <div
+                key={index}
+                data-tomou={tomei}
+                className="flex gap-2 justify-center items-center text-[12px] font-medium rounded-full bg-[#1E2C48] p-1 w-[80px] text-[#375cc3] data-[tomou=true]:text-[#16A34A]"
+              >
+                <CiClock2 className="text-[#3B82F6]" />
+                <span>{horario}</span>
+                {tomei && <FaCheck className="text-[#16A34A]" />}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex gap-2  items-center font-medium rounded-full bg-[#1E2C48] p-1 pl-6 mb-4 md:m-w-[200px]">
+          <CiClock2 className="text-[#3B82F6]" />
+          <div className="flex ">
+            <p> Próxima dose: {tomei ? <p> Parabens </p> : proximaDose} </p>
+          </div>
+        </div>
+
+
+        <div className="flex px-2 gap-4 w-full">
+          <Dialog>
+            <DialogTrigger asChild>
+              <div className="flex flex-1 items-center justify-center w-full gap-2 border border-gray-300 rounded-sm p-2 cursor-pointer hover:bg-[#04102E] min-h-[40px]">
+                <FaRegEdit />
+                <p>Editar</p>
+              </div>
+            </DialogTrigger>
+
+            <DialogContent
+            className='bg-[#1F2937] flex flex-col gap-4'>
+              <DialogHeader>
+                <DialogTitle> Edite o medicamento </DialogTitle>
+                <DialogDescription>
+                    <form>
+                        <div className="flex flex-col justify-start pl-2">
+                            <label className="font-medium pb-1"> Nome do medicamento </label>
+                            <input
+                                type="text"
+                                placeholder={name}
+                                onChange={() => {}} // Adicione um handler real para atualizar o nome
+                                className="bg-[#020817] rounded-md p-2 w-full mb-4 focus:outline-none focus:border-1 focus:border-[#1D4ED8] placeholder:text-[#737882]"
+                            />
                         </div>
-                           
-                        )) }
-                    </div>
-                    
-                    
-                </div>
 
-                <div
-                className='flex gap-2  items-center font-medium rounded-full bg-[#1E2C48] p-1 pl-6 mb-4 md:m-w-[200px]'>
-                    <CiClock2
-                    className='text-[#3B82F6]'/>
-                    <div
-                    className="flex ">
-                        <p> Próxima dose: { tomei ? <p> Parabens </p> : proximaDose } </p>
-                    </div>
-                    
-                </div> 
+                        <div className="flex flex-col pl-2">
+                            <label className="font-medium pb-1"> Dosagem </label>
+                            <input
+                                type="number"
+                                placeholder={dosagem.toString()}
+                                onChange={() => {}} // Adicione um handler real para atualizar a dosagem
+                                className="bg-[#020817] rounded-md p-2 w-full mb-4 focus:outline-none focus:border-1 focus:border-[#1D4ED8] placeholder:text-[#737882]"
+                            />
+                        </div>
 
-                <div
-                className="flex px-2 gap-4 w-full">
-                    <div
-                    className="flex items-center justify-center w-full gap-2 mb-2 border  border-gray-300 rounded-sm p-2 cursor-pointer hover:bg-[#04102E]">
-                        <FaRegEdit/>
-                        <p> editar </p>
-                    </div>
+                        <div className="flex flex-col pl-2">
+                            <label className="font-medium pb-1"> Horários </label>
+                            {horariosSalvos.map((horario, index) => (
+                                <input
+                                    key={index}
+                                    placeholder={horario}
+                                    type="time"
+                                    onChange={() => {}} // Adicione um handler real para atualizar o horário
+                                    required
+                                    className="p-2 bg-[#020817] rounded-md w-full mb-4 focus:outline-none focus:border-1 focus:border-[#1D4ED8] placeholder:text-[#737882]"
+                                />
+                            ))}
+                        </div>
 
-                    <div
-                    className="flex items-center justify-center w-full gap-2 mb-2 bg-[#16A34A] rounded-sm text-[#FFFFFF] p-2 cursor-pointer hover:bg-[#15803D]"
-                    onClick={() => setTomei(true)}>
-                        <FaCheck/>
-                        <p> tomei </p>
-                    </div>
+                        <div>
+                            <p
+                                className="bg-[#020817] rounded-md p-2 w-full mb-4 flex justify-center items-center gap-2 cursor-pointer hover:bg-[#1D4ED8] hover:text-white"
+                                onClick={() => {
+                                    // Adicionar novo horário
+                                }}
+                            >
+                                <CiClock2 />
+                                <span className="font-medium text-center text-[14px]"> Adicionar horário </span>
+                            </p>
+                        </div>
 
-                </div>
-            </article>
-    )
+                        <button
+                            type='submit'
+                            className='bg-[#3B82F6] text-white mb-2 p-2 rounded-lg w-full cursor-pointer hover:bg-[#1D4ED8] hover:text-white'
+                        >
+                            Salvar Medicamento
+                        </button>
+                    </form>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <div
+            className="flex flex-1 items-center justify-center w-full gap-2 bg-[#16A34A] rounded-sm text-[#FFFFFF] p-2 cursor-pointer hover:bg-[#15803D] min-h-[40px]"
+            onClick={() => setTomei(true)}
+          >
+            <FaCheck />
+            <p>tomei</p>
+          </div>
+        </div>
+      </article>
+    );
 }
